a455cd9652114c6767bfc1b51f7b2dbc8e08baf5
André Filho <andre.00000849261@unicap.br>
2026-09-19 10:39:01 -0300

    classe casa

--- POO_STARt.py
+++ POO_STARt.py
@@ -1,1 +1,12 @@
-print("Hello world")
+class Casa:
+    def __init__(self, cor):
+        self.cor = cor
+
+    def mostrar_cor(self):
+        print(f"A cor da casa e: {self.cor}")
+
+casa1 = Casa('Azul')
+casa2 = Casa('Vermelho')
+
+print("\nCasa1:\n")
+casa1.mostrar_cor()
