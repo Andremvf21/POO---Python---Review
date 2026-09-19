a0dd61d5176475a8ca9b47f211aafd58bfd8dd15
André Filho <andre.00000849261@unicap.br>
2026-09-19 10:43:28 -0300

    adicionando quartos

--- POO_STARt.py
+++ POO_STARt.py
@@ -1,12 +1,24 @@
 class Casa:
-    def __init__(self, cor):
+    def __init__(self, cor, quartos):
         self.cor = cor
+        self.quartos = quartos
 
     def mostrar_cor(self):
         print(f"A cor da casa e: {self.cor}")
 
-casa1 = Casa('Azul')
-casa2 = Casa('Vermelho')
+    def mostrar_quartos(self):
+        print(f"Essa casa tem {self.quartos} quartos")
+
+casa1 = Casa('Azul', 4)
+casa2 = Casa('Vermelho', 6)
+
 
 print("\nCasa1:\n")
 casa1.mostrar_cor()
+casa1.mostrar_quartos()
+
+print("\nCasa2:\n")
+casa2.mostrar_cor()
+casa2.mostrar_quartos()
+
+
